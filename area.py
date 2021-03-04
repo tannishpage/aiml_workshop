7dcabf8674fb55230c8618b2acacbc01c51c9670
Aravind Punugu <tannishpage@d-i89-253-144.student.eduroam.uq.edu.au>
2021-03-04 18:23:59 +1000

    Added a function to calculate the area of rectangles

--- area.py
+++ area.py
@@ -1,9 +1,16 @@
 def area_triangle(base, height):
-    return 0.5*base*height
+    area = 0.5*base*height
+    return area
 
 def area_square(side_length):
-    return side_length * side_length
+    area = side_length * side_length
+    return area
+
+def area_rectangle(width, length):
+    area = width * length
+    return area
 
 
 print("Area of triangle with base length 1m and height 2m is:", area_triangle(1, 2), "m^2")
 print("Area of square with side length of 2m is:", area_square(2), "m^2")
+print("Area of rectangle with width of 2m and length of 3m is:", area_rectangle(2, 3), "m^2")
