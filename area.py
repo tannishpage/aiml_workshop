dab375753b28c2716dada3196a9b846a2f9f38b6
Aravind Punugu <tannishpage@d-i89-253-144.student.eduroam.uq.edu.au>
2021-03-04 18:21:04 +1000

    adding project files

---
+++ area.py
@@ -0,0 +1,9 @@
+def area_triangle(base, height):
+    return 0.5*base*height
+
+def area_square(side_length):
+    return side_length * side_length
+
+
+print("Area of triangle with base length 1m and height 2m is:", area_triangle(1, 2), "m^2")
+print("Area of square with side length of 2m is:", area_square(2), "m^2")
