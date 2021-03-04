e7e07d316bb50cd8aa915850a5b5c99fde363620
Aravind Punugu <tannishpage@d-i89-253-144.student.eduroam.uq.edu.au>
2021-03-04 18:25:32 +1000

    Added a function to calculate the area of circle using pi from the math library

--- area.py
+++ area.py
@@ -1,16 +1,22 @@
+import mathn
+
 def area_triangle(base, height):
     area = 0.5*base*height
     return area
 
 def area_square(side_length):
-    area = side_length * side_length
+    area = side_length ** 2
     return area
 
 def area_rectangle(width, length):
     area = width * length
     return area
 
+def area_circle(radius):
+    area = math.pi * (radius ** 2)
+
 
 print("Area of triangle with base length 1m and height 2m is:", area_triangle(1, 2), "m^2")
 print("Area of square with side length of 2m is:", area_square(2), "m^2")
 print("Area of rectangle with width of 2m and length of 3m is:", area_rectangle(2, 3), "m^2")
+print("Area of circle with radius of 2m is:", area_circle(2), "m^2")
